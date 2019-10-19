b7e0c247a4c8cf04af0330b7676d0c124fa4deb7
nathan <nate.richards96@gmail.com>
2019-10-19 16:44:53 -0700

    Added Camera Prefab

---
+++ KillBox/Assets/Scripts/UI/LevelScreen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelScreen : MonoBehaviour {
+
+    public Slider healthSlider;
+    public Slider damageSlider;
+    public float damageDecrement;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateDamageBar();
+        if (Input.anyKey)
+        {
+            DamageHealthBar(.25f);
+
+        }
+    }
+
+
+    void DamageHealthBar(float damageVal)
+    {
+        healthSlider.value -= damageVal;
+    }
+
+    void UpdateDamageBar()
+    {
+        float targetHealthBar = healthSlider.value;
+        if (damageSlider.value >= targetHealthBar)
+        {
+            damageSlider.value -= damageDecrement;
+        }
+        else if (damageSlider.value < targetHealthBar)
+        {
+            damageSlider.value = targetHealthBar;
+        }
+    }
+}
