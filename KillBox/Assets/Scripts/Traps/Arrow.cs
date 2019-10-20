5b9e879f2a832445f869a4300aabf7287e66c942
Thana Sithisombath <ThanaSithisombath@gmail.com>
2019-10-19 17:06:43 -0700

    added Traps

--- KillBox/Assets/Scripts/Traps/Arrow.cs
+++ KillBox/Assets/Scripts/Traps/Arrow.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour {
 
+    int count = 0;
+    public int lifetime;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,6 +15,11 @@ public class Arrow : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         this.GetComponent<Transform>().position += new Vector3(0f, -0.01f, 0f);
+        count++;
+        if (count > lifetime)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     public void OnCollisionEnter2D(UnityEngine.Collision2D collision)
